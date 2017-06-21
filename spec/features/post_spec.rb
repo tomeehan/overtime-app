f45638d118d8d9b8f54c67ad0ea3cfbc87f50060
tomeehan <tom.meehan@me.com>
2017-06-21 15:02:02 +0100

    Lists posts on posts_index

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,38 @@
 require 'rails_helper'
 
-describe 'navigate' do 
+describe 'navigate' do
+  before do
+    user = User.create(email: "test@me.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Kevin", last_name: "Spacey")
+    login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached' do 
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'has a title of Posts' do 
-      visit posts_path
+    it 'has a title of Posts' do
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
-    before do 
-      user = User.create(email: "test@me.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Kevin", last_name: "Spacey")
-      login_as(user, :scope => :user)
+    before do
       visit new_post_path
     end
 
-    it 'has a new form that can be reached' do 
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
