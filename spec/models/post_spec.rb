fb296e65dd10b8fc3a38d71992d68d838b9732c8
tomeehan <tom.meehan@me.com>
2017-06-27 00:40:20 +0100

    Styling for pricing page, and tests passing.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe "Creation" do
+RSpec.describe User, type: :model do
+  describe "creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
     end
 
-    it 'can be created' do  
-      expect(@post).to be_valid
+    it "can be created" do
+      expect(@user).to be_valid
     end
 
-    it 'cannot be created without a date and rationale' do
-      @post.date = nil
-      @post.rationale = nil
-      expect(@post).to_not be_valid
+    it "cannot be created without first_name, last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
     end
   end
 end
