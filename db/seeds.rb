c7758b4a187044706115237d7e83b7a2beea548b
tomeehan <tom.meehan@me.com>
2017-06-27 00:51:28 +0100

    Creates user and defined user_id when creating posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "test@me.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Kevin", last_name: "Spacey")
+puts '1 user created'
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 posts have been created."
