bf4cdc76997232f6e96660cc88005a90f39f0ae5
tomeehan <tom.meehan@me.com>
2017-06-30 17:40:20 +0100

    Implemented edit functionality for status on post.

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -8,12 +8,13 @@ class PostDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo.with_options(searchable: false)
-    id: Field::Number.with_options(searchable: false)
+    user: Field::BelongsTo.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
     date: Field::DateTime.with_options(searchable: false),
     rationale: Field::Text,
-    created_at: Field::DateTime.with_options(searchable: false)
-    updated_at: Field::DateTime.with_options(searchable: false)
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true)
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,6 +24,7 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
@@ -32,6 +34,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 @user = User.create(email: "test@me.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Kevin", last_name: "Spacey")
 puts '1 user created'
 
+@admin_user = AdminUser.create(email: "admin@me.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Admin", last_name: "Admin")
+puts '1 admin user created'
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
